efe63d3d581e5ead62f7e295964687df0f58a40e
Jonnathan-Git <jonnathangrclr@gmail.com>
2024-04-25 12:09:52 -0600

    new functionalities have been added and the aesthetics of the application has been improved.

--- config/Config.js
+++ config/Config.js
@@ -3,7 +3,7 @@ import fs from 'fs';
  * Contains the raw data read from the config.json file.
  * @type {Buffer}
  **********************************************************/
-const rawData = fs.readFileSync('./src/config/config.json');
+const rawData = fs.readFileSync('./config/config.json');
 const Config = JSON.parse(rawData).development;
 
 export default Config;
